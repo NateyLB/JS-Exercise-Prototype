17f9f0022133179cd8e5a1e6af9043256963e036
Nathan <nateyl.b@gmail.com>
2020-02-26 10:18:37 -0800

    Baby Passed

--- index.js
+++ index.js
@@ -101,6 +101,10 @@ function Baby(name, age, favoriteToy) {
   Person.call(this, name, age, favoriteToy);
   this.favoriteToy = favoriteToy
 }
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
 
 /* 
   TASK 4
